8714100586d1d1eb60dbb7920556bcd6ba62901a
SaketSaurab <100rabhsinghalld@gmail.com>
2021-08-03 09:18:04 +0530

    3/8/21

---
+++ JavaAssignment/src/MethodToCheckStringIsPalindromeOrNot.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class MethodToCheckStringIsPalindromeOrNot {
+    static boolean palindromeOrNot(String str) {
+        int a = 0, b = str.length() - 1;
+        while (a < b) {
+            if (str.charAt(a) != str.charAt(b)) {
+                return false;
+
+            }
+            a++;
+            b--;
+
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        if (palindromeOrNot(str1))
+            System.out.println("Palindrome");
+         else System.out.println("Not Palindrome");
+    }
+}
